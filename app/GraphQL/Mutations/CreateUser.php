c1a23393bacc8f3fc9e551fb04315a5c02798b89
Roberto Pichardo Mier <rpichardomier23@gmail.com>
2025-12-19 11:41:26 -0600

    Crear la mutación en lugar de crear desde el esquema

---
+++ app/GraphQL/Mutations/CreateUser.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+
+final readonly class CreateUser
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        // TODO implement the resolver
+        $user = User::create($args);
+
+        return $user;
+    }
+}
