44724cf5c9241baa82cb6c0177ea90022e874a4a
Roberto Pichardo Mier <rpichardomier23@gmail.com>
2026-01-23 20:23:46 -0600

    Se agrego una variable que almacena los productos del carrito que se van a comprar usando el id, y solo se borran esos cuando se confirma la orden

--- app/GraphQL/Mutations/NewOrder.php
+++ app/GraphQL/Mutations/NewOrder.php
@@ -71,7 +71,11 @@ final readonly class NewOrder
             $cart = Cart::where('user_id', $user->id)->first();
 
             if ($cart) {
-                CartItem::where('cart_id', $cart->id)->delete();
+
+                $boughtProductsIds = collect($args['items'])->pluck('product_id');
+                CartItem::where('cart_id', $cart->id)
+                            ->whereIn('product_id', $boughtProductsIds)
+                            ->delete();
             }
 
             return $order;
