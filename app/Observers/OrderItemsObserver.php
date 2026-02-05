34735b3fd55f3a3458ac0bc599ad540736710d19
Ragly-24 <reusc.2000@gmail.com>
2026-02-05 15:50:09 -0600

    feat: Es el observador que estara monitorendo cuando OrderItems recibe informacion.

--- app/Observers/OrderItemsObserver.php
+++ app/Observers/OrderItemsObserver.php
@@ -5,33 +5,33 @@ namespace App\Observers;
 use App\Models\OrderItems;
 use App\Models\Product;
 use App\Notifications\SaleAlert;
+use App\Notifications\PurchaseSuccess;
 use Illuminate\Support\Facades\Auth;
 
 class OrderItemsObserver
 {
-    /**
-     * Handle the OrderItems "created" event.
-     */
     public function created(OrderItems $orderItem): void
     {
-
         $product = Product::find($orderItem->product_id);
 
         if ($product) {
-
             $seller = $product->user;
-
-
             $buyer = Auth::user();
 
 
             if ($seller && $buyer && $seller->id !== $buyer->id) {
-
-
                 $seller->notify(new SaleAlert(
                     $product,
                     $orderItem->quantity,
                     $buyer->name
+                ));
+            }
+
+
+            if ($buyer) {
+                $buyer->notify(new PurchaseSuccess(
+                    $product,
+                    $orderItem->quantity
                 ));
             }
         }
